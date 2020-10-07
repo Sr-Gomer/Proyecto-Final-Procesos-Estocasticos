3f1b505d3d89f9d2a1b4956891c89d7d6e8a8734
tridoxx <julianhoyos91@gmail.com>
2020-10-07 17:20:29 -0500

    Update bd.py

--- base_De_datos/bd.py
+++ base_De_datos/bd.py
@@ -6,7 +6,7 @@ from PyQt5.QtSql import QSqlDatabase,QSqlQuery, QSqlTableModel
 
 
 db = QSqlDatabase.addDatabase('QMYSQL')#driver de la base de datos en este caso mysql tambien funciona para mariadb
-db.setHostName("186.147.72.83")#servidor de conexion de la base de datos, por defecto el puerto es 3306
+db.setHostName("192.168.100.243")#servidor de conexion de la base de datos, por defecto el puerto es 3306
 db.setDatabaseName("casino")#nombre de la base de datos
 db.setUserName("usuariocasino")#usuario para conectarse a la base de datos
 db.setPassword("casino")#contraseña del usuario para la base de datos
@@ -48,3 +48,29 @@ def traerValor():
      db.close()
      #print("soy total",total)
      return total
+
+def insertarDatos(total_transaccion,detalle_transacion):
+ estado=db.open()
+ if estado == False:
+  print("no se pudo conectar")
+  #QMessageBox.warning("error",db.lastError().text(),QMessageBox.Discard)
+  print("error",db.lastError().text())
+ else:
+  print("conexion a base de datos correcta")
+  #total_transaccion=entradaganancia.text()
+    #aqui se hace una inserccion a la base da datos en la tabla transacion la tabla se define despues del "insert into "
+  sql="INSERT INTO transacciones(total_transaccion,detalle_transacion) VALUES(:total_transaccion,:detalle_transacion)"
+  consulta=QSqlQuery()
+  consulta.prepare(sql)
+  consulta.bindValue(":total_transaccion",total_transaccion)
+  consulta.bindValue(":detalle_transacion",detalle_transacion)
+  #print(total_transaccion)
+  #aqui se ejecuta la consulta
+  estado=consulta.exec_()
+  if estado==True:
+   #QMessageBox.warning("correcto","datos guardados",QMessageBox.Discard)
+   print("correcto","datos guardados")
+  else:
+   #QMessageBox.warning("error",db.lastError().text(),QMessageBox.Discard)
+   print("error",db.lastError().text())
+   db.close()
