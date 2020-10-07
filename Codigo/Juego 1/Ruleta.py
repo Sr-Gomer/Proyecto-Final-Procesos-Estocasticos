3ab872f29745d0956b7f23048d3fec9d28a5cff9
Kolark <47009873+Kolark@users.noreply.github.com>
2020-10-07 02:54:22 -0500

    solucionealgunascositas

--- Codigo/Juego 1/Ruleta.py
+++ Codigo/Juego 1/Ruleta.py
@@ -1,23 +1,31 @@
+#Copiar el el script y no el .py, El .py lo llaman igual que su juego 
 
 import sys,re
 from PyQt5 import uic, QtWidgets, QtGui
 
 from PyQt5.QtWidgets import QMessageBox
 
-uiFile = "./xd4.ui" # Nombre del archivo aquí. Debe estar en la misma carpeta
-Ui_Dialog, QtBaseClass = uic.loadUiType(uiFile)
+uiFile = "./ruletaui.ui" # Nombre del archivo aquí. Debe estar en la misma carpeta
+ruedaImage = "./imagenejemplo.png"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(uiFile)
 
 
-class UIWindow(QtWidgets.QDialog, Ui_Dialog):
-    def init(self):
-        QtWidgets.QMainWindow.init(self)
-        Ui_Dialog.init(self)
+class UIWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
         self.setupUi(self)
-       
+        self.img = QtGui.QPixmap(ruedaImage) # carga la imagen
+        self._imagen.setPixmap(self.img) #Le pone la imagen al label
+        self._Lanzar.clicked.connect(self.ejemplobotonprint)#Conecta el boton a el metodo
+    def ejemplobotonprint(self):
+        print("Hola mundo")
+
 def Run():
     app =  QtWidgets.QApplication(sys.argv)
     window = UIWindow()
     window.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
     
-Run() 
+
+Run()#Descomentar esta linea cuando se quiera probar individualmente.
